cf6dd1d086c8a96e0fffc2b4b0c35338d1513e04
Layon Ferreira <layon1993@gmail.com>
2019-05-07 13:42:37 +0100

    Add ability to snooze and make the alarm sound repeat in loop

--- romodoro.rb
+++ romodoro.rb
@@ -1,15 +1,21 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'bundler/setup'
 require 'colorize'
+require 'open3'
 
 class Romodoro
   WORK_TIME = 25 * 60 # time of a work sprint in seconds
-  SHORT_BREAK = 5 * 60 # shor break in seconds
+  SHORT_BREAK = 5 * 60 # short break in seconds
   LONG_BREAK = 15 * 60 # long break in seconds
-  REPETITIONS = 4 # repetitions before a long break
+  SNOOZE_BREAK = 5 * 60 # snooze break in seconds
 
-  ALERTER_PATH = './bin/alerter'.freeze
-  SOUND_PATH = './bin/analog-watch-alarm_daniel-simion.mp3'.freeze
+  REPETITIONS = 4 # repetitions before a long break
+  ALERTER_PATH = './bin/alerter'
+  SOUND_PATH = './bin/analog-watch-alarm_daniel-simion.mp3'
+  SNOOZE_COMMAND = 'Snooze'
+  START_COMMAND = 'Start'
   attr_reader :continue
 
   def initialize
@@ -31,6 +37,33 @@ class Romodoro
 
   def start_task(seconds, message, color)
     notify(message)
+    start_timer(seconds, message, color)
+  end
+
+  def notify(message)
+    response = nil
+    while response != START_COMMAND
+      pid = start_alarm
+      response = notification_command(message)
+      kill_alarm(pid)
+      start_timer(SNOOZE_BREAK, 'Snooze', :red) if response == SNOOZE_COMMAND
+    end
+  end
+
+  private
+
+  def start_alarm
+    fork do
+      exec "while :; do afplay #{SOUND_PATH}; done"
+    end
+  end
+
+  def kill_alarm(pid)
+    Process.kill('TERM', pid)
+    IO.popen('killall afplay')
+  end
+
+  def start_timer(seconds, message, color)
     puts "#{message} #{seconds / 60} minutes.".colorize(color)
     puts ''
     seconds.times do |i|
@@ -39,10 +72,14 @@ class Romodoro
     end
   end
 
-  def notify(message)
-    IO.popen("afplay #{SOUND_PATH}")
-    system("#{ALERTER_PATH} -title Romodoro -message \"#{message}\" -closeLabel Start -actions Start")
-    IO.popen('killall afplay')
+  def notification_command(message)
+    command = "#{ALERTER_PATH} -title Romodoro -message \"#{message}\" " \
+                "-closeLabel #{SNOOZE_COMMAND} -actions #{START_COMMAND}"
+    response = nil
+    ::Open3.popen3(command) do |_stdin, stdout, _stderr, _wait_thr|
+      response = stdout.read
+    end
+    response
   end
 end
 
